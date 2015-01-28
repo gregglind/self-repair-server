d679e838bd3f27e8ae543a35aa54a9df6159fc85
Gregg Lind <gregg.lind@gmail.com>
2015-01-28 11:49:21 -0600

    Karma with coverage, used coverage-webpack replacement

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,14 @@
 // Karma configuration
 // Generated on Tue Jan 27 2015 14:16:43 GMT-0600 (CST)
 
+
+/* features of this Karma conf:
+- each test is run as a webpack
+-
+*/
+// http://npm.taobao.org/package/istanbul-instrumenter-loader
+
+
 module.exports = function(config) {
   config.set({
 
@@ -15,7 +23,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      //{pattern: 'src/*.js', included: false},
+      //{pattern: 'src/*.js', included: false}, // not needed, get wepbacked.
       {pattern: 'test/*.js', included: true}
     ],
 
@@ -26,12 +34,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        //'src/*.js': ['webpack'],
-        'test/*': ['webpack']  // not recursive!
+        'test/*': ['webpack'] // , 'coverage']  // not recursive!
     },
 
     plugins: [
         //require("karma-webpack")
+        'karma-coverage',
         'karma-webpack',
         'karma-mocha',
         'karma-firefox-launcher'
@@ -52,10 +60,23 @@ module.exports = function(config) {
       }
     },
 
+
+    //http://npm.taobao.org/package/istanbul-instrumenter-loader
+    webpack: {
+      module: {
+        //loaders: [ ... ],
+        postLoaders: [ { // << add subject as webpack's postloader
+          test: /\.js$/,
+          exclude: /(test|node_modules|bower_components)\//,
+          loader: 'istanbul-instrumenter'
+        } ]
+      },
+    },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'dots'], //'coverage'],
+    reporters: ['progress', 'dots', 'coverage'],
 
 
     // web server port
